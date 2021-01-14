cd2865b84be60e25ec915da12642511b14f6b149
Zeller <matheo.zeller@gmail.com>
2021-01-14 17:59:38 +0100

    feat(dark-mode): add dark-mode to project

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,17 @@ import './styles/app.scss';
 
 // any JS you import will output into a single css file (app.js in this case)
 import './scripts/app.js';
+
+
+/*
+Light-Theme function
+ */
+
+// Select the button
+const btn = document.querySelector('.btn-toggle');
+
+// Listen for a click on the button
+btn.addEventListener('click', function() {
+    // Then toggle (add/remove) the .dark-theme class to the body
+    document.body.classList.toggle('light-theme');
+})
