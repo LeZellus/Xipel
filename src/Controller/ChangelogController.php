72e0a8fe3387779adb282c79257ecd5ffcd78cce
Zeller <matheo.zeller@gmail.com>
2021-02-09 11:27:55 +0100

    delete(changelog): remove useless show method

--- src/Controller/ChangelogController.php
+++ src/Controller/ChangelogController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Changelog;
 use App\Form\ChangelogType;
 use App\Repository\ChangelogRepository;
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -33,29 +34,18 @@ class ChangelogController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $changelog->setCreatedAt(new \DateTime());
+            $changelog->setCreatedAt(new DateTime());
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($changelog);
             $entityManager->flush();
 
-            $this->addFlash("success", "Le changelog à été mis à jour");
+            $this->addFlash("success", "Le changelog à été ajouté");
             return $this->redirectToRoute('changelog_index');
         }
 
         return $this->render('changelog/new.html.twig', [
             'changelog' => $changelog,
             'form' => $form->createView(),
-        ]);
-    }
-
-    /**
-     * @param Changelog $changelog
-     * @return Response
-     */
-    public function show(Changelog $changelog): Response
-    {
-        return $this->render('changelog/show.html.twig', [
-            'changelog' => $changelog,
         ]);
     }
 
@@ -83,11 +73,10 @@ class ChangelogController extends AbstractController
     }
 
     /**
-     * @param Request $request
      * @param Changelog $changelog
      * @return Response
      */
-    public function remove(Request $request, Changelog $changelog): Response
+    public function remove(Changelog $changelog): Response
     {
         $entityManager = $this->getDoctrine()->getManager();
         $entityManager->remove($changelog);
